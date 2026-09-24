d9535eb995f14479ddf6e6d8ba653592b46bffac
Shashank Kumar Gupta <crushcastic@gmail.com>
2026-09-24 21:33:53 +0530

    fix: add newline at end of file and format Expense struct

--- internal/task/task.go
+++ internal/task/task.go
@@ -6,7 +6,17 @@ import (
 
 type Expense struct {
     ID          int     	`json:"id"`
-    Date        time.Time  	`json:"date"`
+    Date        time.Time 	`json:"date"`
     Description string  	`json:"description"`
     Amount      float64 	`json:"amount"`
+}
+
+func NextID(expenses []Expense) int {
+    maxID := 0
+    for _, expense := range expenses {
+        if expense.ID > maxID {
+            maxID = expense.ID
+        }
+    }
+    return maxID + 1
 }
